214c113dea11736246c6bd8522c3e219b321c5bd
Anshul-ARK <kanshulreddy93@gmail.com>
2024-10-17 15:50:51 +0530

    first commit

---
+++ src/MainPackage/Main.java
@@ -0,0 +1,49 @@
+package MainPackage;
+import java.util.Scanner;
+
+import passwordPackage.passwords;
+
+import java.lang.InterruptedException;
+public class Main {
+   public static void main(String[] args) {
+	   Scanner sc = new Scanner(System.in);
+	   System.out.println("Welcome to the portal ! continue as ");
+	   int p = -1;
+	   while(p == -1) 
+	   	{
+		   System.out.println(" 1)admin\n 2)professor\n 3)student\n 4)Exit");
+		   System.out.println("choose the role from above");
+		   int k = sc.nextInt();
+		   		if(k < 1 || k > 4)
+		   		{
+		   			System.out.println("Invalid choice ! try again");
+		   			try {
+		   				Thread.sleep(100);
+		   				}
+		   			catch(InterruptedException ex) {
+		   				ex.printStackTrace();
+		   				}
+		   		}
+		   		else
+		   		{
+		   				p = k;
+		   		}
+	   	}
+	   switch(p) {
+	   		case 1:
+	   			//admin login
+	   			break;
+	   		case 2:
+	   		    //professor login
+	   			break;
+	   		case 3:
+	   			//student login
+	   			break;
+	   		case 4:
+	   			//Exit
+	   			System.out.println("Have a nice day! You have successfully exited the program.");
+	   			System.exit(0);
+	   			return ;
+	   }
+   }
+}
---
+++ src/adminPackage/admin.java
@@ -0,0 +1,45 @@
+package adminPackage;
+import java.util.Scanner;
+public class admin{
+   public static void login() {
+	   Scanner sc = new Scanner(System.in);
+	   String username,pass;
+	   System.out.println("Enter your username");
+	   username = sc.nextLine();
+	   sc.nextLine();
+	   System.out.println("Enter your password");
+	   pass = sc.nextLine();
+		if(username.equals("admin") && pass.equals("admin")) 
+		{
+			   System.out.println("You have successfully entered the admin panel");
+			   //admin managment for records and that stuff
+		}
+		else
+		{
+			while(true)
+			{
+				System.out.println("Do you want to try again !? Y / N");
+				String s = sc.nextLine();
+				if(s.equalsIgnoreCase("y"))
+				{
+					admin.login();
+					return;
+				}
+				else if(s.equalsIgnoreCase("N"))
+				{
+					System.out.println("1)Main page \n  2)exit");
+					int choice = sc.nextInt();
+				 		switch(choice)
+				 		{
+				           
+				 		}
+				 	return;
+				}
+			   else
+			   {
+				 System.out.println("Invalid choice ! choose again");
+			   }
+			}
+		}
+   }
+}
